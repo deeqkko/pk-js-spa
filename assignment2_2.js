8c91729fa146031647a7d05556319b0358e9480f
Kimmo Koivusalo <deeq@deeqmac.local>
2018-08-28 16:58:57 +0300

    assignment2_2 added to repo

---
+++ assignment2_2.js
@@ -0,0 +1,9 @@
+//Returns a last character of given string [s]
+
+function lastCharacter(s){return(s[s.length-1])};
+
+/*
+Strings Exercise for next lesson
+
+make function lastCharacter(s), which always returns the last character of string s
+*/
